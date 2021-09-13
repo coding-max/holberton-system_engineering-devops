4b33f641608128ca0358a8c2fe895333e6181dcb
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-09-13 20:08:18 +0000

    269#1 added file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""using the JSONPlaceholder API:
+   returns information about the TODO list progress for a given employee ID"""
+
+import csv
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(argv[1])).json()
+    todo = requests.get(url + "todos", params={"userId": argv[1]}).json()
+    tasks = []
+    for task in todo:
+        if task.get('completed') is True:
+            tasks.append(task.get('title'))
+
+    with open(str(argv[1]) + ".csv", "w") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            writer.writerow([int(argv[1]), user.get('username'),
+                             task.get('completed'), task.get('title')])
